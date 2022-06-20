48cc37375f1f392066b9400cb447e3485c0a4451
SimJoonYeol <12195302@inha.edu>
2022-06-21 01:06:33 +0900

    jjj

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import copy
+
 from rrt_modify import RRT
 import heapq
 import random
@@ -5,7 +7,6 @@ import math
 
 
 class SSSP:
-
     class Node:
         def __init__(self, x, y):
             self.x = x
@@ -15,47 +16,23 @@ class SSSP:
             self.parent = None
 
     def __init__(self, _obstacle_list, _robot_radius):
-        self.obstacle_list = _obstacle_list
-        self.robot_radius = _robot_radius
         self.roadmap = []
         self.min_rand = 0
         self.max_rand = 30
-        self.sampling_m = 10
+        self.sampling_m = 5
         self.threshold_dis = 3
         self.rrt = RRT(
             rand_area=[0, 30],
-            obstacle_list=self.obstacle_list,
-            robot_radius=self.robot_radius,
+            obstacle_list=_obstacle_list,
+            robot_radius=_robot_radius,
             play_area=[0, 30, 0, 30]
         )
 
-    def get_random_node(self):
-        rnd = self.Node(
-            random.uniform(self.min_rand, self.max_rand),
-            random.uniform(self.min_rand, self.max_rand))
-        return rnd
+    def collide(self):
+        return False
 
-    def steer(self, from_node, to_node, threshold_length=float("inf")):
-        new_node = self.Node(from_node.x, from_node.y)
-        d, theta = self.calc_distance_and_angle(new_node, to_node)
-
-        if threshold_length > d:
-            return None
-
-        new_node.x = d * math.cos(theta)
-        new_node.y = d * math.sin(theta)
-
-        new_node.parent = from_node
-
-        return new_node
-
-    @staticmethod
-    def calc_distance_and_angle(from_node, to_node):
-        dx = to_node.x - from_node.x
-        dy = to_node.y - from_node.y
-        d = math.hypot(dx, dy)
-        theta = math.atan2(dy, dx)
-        return d, theta
+    def calculate_score(self):
+        return 0
 
     def search(self, start_list, goal_list):
         roadmaps = list()
@@ -69,7 +46,8 @@ class SSSP:
             n = len(q_init)
             i_next = 1
             parent = None
-            heapq.heappush(frontier, (0, q_init, i_next, parent))# score, Q, next, parent
+            # score와 로봇들의 현재 위치, next, parent
+            heapq.heappush(frontier, (0, q_init, i_next, parent))  # score, Q, next, parent
             explored = list()
             explored.append((q_init, i_next))
 
@@ -84,15 +62,21 @@ class SSSP:
                 else:
                     return s
 
-                i = (s[2] - 1) # zero based
+                i = (s[2] - 1)  # zero based
                 q_state_from = s_q[i]
 
                 # vertex expansion via sampling
                 for _ in range(self.sampling_m):
-                    q_state_rand = self.get_random_node()
-                    q_state_new = self.steer(q_state_from, q_state_rand, self.threshold_dis)
-                    if q_state_new:
+                    # 랜덤 노드 생성 후 stree로 새로운 new node 생성
+                    q_state_rand = self.rrt.get_random_node()
+                    q_state_new = self.rrt.steer(q_state_from, q_state_rand, self.threshold_dis)
+
+                    # 장애물에 걸리는지 확인 후 로드맵에 추가 및 edge 연결
+                    if self.rrt.check_if_outside_play_area(q_state_new, self.rrt.play_area) and \
+                            self.rrt.check_collision(q_state_new, self.rrt.obstacle_list, self.rrt.robot_radius):
                         roadmaps[i].append(q_state_new)
+                        q_state_new.edge.append(q_state_from)
+                        q_state_from.edge.append(q_state_new)
 
                 # search node expansion
                 if i != n:
@@ -100,15 +84,23 @@ class SSSP:
                 else:
                     i_prime = 1
 
+                for q_state_to in q_state_from.edge:
+                    q_prime = copy.deepcopy(s_q)
+                    q_prime[i] = q_state_to
+                    if (q_prime, i_prime) not in explored and not self.collide():
+                        score = self.calculate_score()
+                        heapq.heappush(frontier, (score, q_prime, i_prime, s))
+                        explored.append((q_prime, i_prime))
+
     def get_random_pos_list(self, robot_n, max_x, max_y):
         pos_list = []
         while len(pos_list) < robot_n:
             pos = (random.randint(0, max_x), random.randint(0, max_y))
             if pos not in pos_list:
-                for (ox, oy, size) in self.obstacle_list:
+                for (ox, oy, size) in self.rrt.obstacle_list:
                     dx = ox - pos[0]
                     dy = oy - pos[1]
-                    if dx * dx + dy * dy <= (size + self.robot_radius) ** 2:
+                    if dx * dx + dy * dy <= (size + self.rrt.robot_radius) ** 2:
                         break
                 else:
                     pos_list.append(pos)
